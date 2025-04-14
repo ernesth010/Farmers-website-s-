94ec3d497c8279385c77447e1af5e88ee112fc88
Muigai Ernest <ernestmbogo7@gmail.com>
2025-04-14 12:36:36 +0300

    progress

---
+++ message.js
@@ -0,0 +1,9 @@
+const update = document.getElementById("farmerForm");
+
+
+update.addEventListener("submit", (event) => {
+    event.preventDefault();
+    alert("message sent successfully");
+    console.log("successfully");
+    
+});
